b004b697131d8c91e13c7f7a77a57662ccd857ab
vantr2 <trongvan.tr@gmail.com>
2021-05-21 16:24:45 +0700

    Chinh lai phan default avt nhan vien

--- client/src/components/quanly/profilenhanvien/ProfileNhanVien.js
+++ client/src/components/quanly/profilenhanvien/ProfileNhanVien.js
@@ -11,7 +11,6 @@ const ProfileNhanVien = () => {
   const username = window.localStorage.getItem("user_name");
 
   const [nhanvienSelected, setNhanVienSelected] = useState([]);
-  const [loiCapPhep, setLoiCapPhep] = useState("");
 
   const [filename, setFileName] = useState("");
   const [avt, setAvt] = useState("");
@@ -36,14 +35,8 @@ const ProfileNhanVien = () => {
         const res = await NhanVienFinder.get(
           `/danh-sach-nhan-vien-theo-acc/${username}`
         );
-        // console.log(res.data.data.nhanvien);
         if (res.data.status === "ok") {
-          if (res.data.data.nhanvien === undefined) {
-            setLoiCapPhep("Tài khoản bạn sử dụng chưa được cấp phép.");
-          } else {
-            setNhanVienSelected(res.data.data.nhanvien);
-            // console.log(res);
-          }
+          setNhanVienSelected(res.data.data.nhanvien);
         }
       } catch (err) {
         console.log(err.message);
@@ -56,6 +49,7 @@ const ProfileNhanVien = () => {
       try {
         const res = await TaiKhoanFinder.get(`/get-avt/${username}`);
         if (res.data.status === "ok") {
+          console.log(res);
           setAvt(res.data.data.nguoidung.avt);
           setFileName(res.data.data.nguoidung.filename);
         }
@@ -102,113 +96,103 @@ const ProfileNhanVien = () => {
 
   return (
     <div>
-      {loiCapPhep !== "" ? (
-        <p className="text-center text-danger">{loiCapPhep}</p>
-      ) : (
-        <div className="mt-5 d-flex ml-5">
-          <div className="d-flex flex-column">
-            <img
-              alt="Avartar"
-              width="300"
-              height="300"
-              className="border border-secondary mb-2"
-              src={avt}
-            />
-            <label
-              style={{
-                backgroundColor: "blue",
-                color: "white",
-                padding: 10,
-                borderRadius: 4,
-                cursor: "pointer",
-                width: "300px",
-                textAlign: "center",
+      <div className="mt-5 d-flex ml-5">
+        <div className="d-flex flex-column">
+          <img
+            alt="Avartar"
+            width="300"
+            height="300"
+            className="border border-secondary mb-2"
+            src={avt}
+          />
+          <label
+            style={{
+              backgroundColor: "blue",
+              color: "white",
+              padding: 10,
+              borderRadius: 4,
+              cursor: "pointer",
+              width: "300px",
+              textAlign: "center",
+            }}
+          >
+            Sửa ảnh đại diện
+            <FileUploader
+              accept="image/*"
+              name={image}
+              storageRef={storage.ref("images")}
+              onUploadStart={() => {
+                setIsUploading(true);
+                setProgress(0);
               }}
-            >
-              Sửa ảnh đại diện
-              <FileUploader
-                accept="image/*"
-                name={image}
-                storageRef={storage.ref("images")}
-                onUploadStart={() => {
-                  setIsUploading(true);
-                  setProgress(0);
-                }}
-                onUploadError={(error) => {
-                  console.log(error);
-                  setIsUploading(false);
-                }}
-                onUploadSuccess={handleUpload}
-                onProgress={(progress) => {
-                  setProgress(progress);
-                }}
-                hidden
-              />
-            </label>
-            {isUploading ? (
-              <progress value={progress} max="100" className="form-control">
-                {" "}
-                {progress}{" "}
-              </progress>
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="flex-grow ml-5 pl-5 mt-3">
-            <h4>
-              Họ và tên:{" "}
-              <span className="font-weight-normal">
-                {nhanvienSelected.name}
-              </span>
-            </h4>
-            <h4>
-              Giới tính:{" "}
-              <span className="font-weight-normal">
-                {RenderGioiTinh(nhanvienSelected.gioitinh)}
-              </span>
-            </h4>
-            <h4>
-              Ngày sinh:{" "}
-              <span className="font-weight-normal">
-                {NormalizeDate(nhanvienSelected.ngaysinh)}
-              </span>
-            </h4>
-            <h4>
-              Chứng minh nhân dân:{" "}
-              <span className="font-weight-normal">
-                {nhanvienSelected.cmnd}
-              </span>
-            </h4>
-            <h4>
-              Số điện thoại:{" "}
-              <span className="font-weight-normal">{nhanvienSelected.sdt}</span>
-            </h4>
-            <h4>
-              Email:{" "}
-              <span className="font-weight-normal">
-                {nhanvienSelected.email}
-              </span>
-            </h4>
-            <h4>
-              Địa chỉ:{" "}
-              <span className="font-weight-normal">
-                {nhanvienSelected.diachi}
-              </span>
-            </h4>
-            <h4>
-              Tài khoản đang sử dụng:{" "}
-              <span className="font-weight-normal">
-                {nhanvienSelected.account}
-              </span>
-            </h4>
-            <DoiMatKhau
-              ten={nhanvienSelected.account}
-              iv={nhanvienSelected.iv}
-              mk={nhanvienSelected.mk}
+              onUploadError={(error) => {
+                console.log(error);
+                setIsUploading(false);
+              }}
+              onUploadSuccess={handleUpload}
+              onProgress={(progress) => {
+                setProgress(progress);
+              }}
+              hidden
             />
-          </div>
+          </label>
+          {isUploading ? (
+            <progress value={progress} max="100" className="form-control">
+              {" "}
+              {progress}{" "}
+            </progress>
+          ) : (
+            ""
+          )}
         </div>
-      )}
+        <div className="flex-grow ml-5 pl-5 mt-3">
+          <h4>
+            Họ và tên:{" "}
+            <span className="font-weight-normal">{nhanvienSelected.name}</span>
+          </h4>
+          <h4>
+            Giới tính:{" "}
+            <span className="font-weight-normal">
+              {RenderGioiTinh(nhanvienSelected.gioitinh)}
+            </span>
+          </h4>
+          <h4>
+            Ngày sinh:{" "}
+            <span className="font-weight-normal">
+              {NormalizeDate(nhanvienSelected.ngaysinh)}
+            </span>
+          </h4>
+          <h4>
+            Chứng minh nhân dân:{" "}
+            <span className="font-weight-normal">{nhanvienSelected.cmnd}</span>
+          </h4>
+          <h4>
+            Số điện thoại:{" "}
+            <span className="font-weight-normal">{nhanvienSelected.sdt}</span>
+          </h4>
+          <h4>
+            Email:{" "}
+            <span className="font-weight-normal">{nhanvienSelected.email}</span>
+          </h4>
+          <h4>
+            Địa chỉ:{" "}
+            <span className="font-weight-normal">
+              {nhanvienSelected.diachi}
+            </span>
+          </h4>
+          <h4>
+            Tài khoản đang sử dụng:{" "}
+            <span className="font-weight-normal">
+              {nhanvienSelected.account}
+            </span>
+          </h4>
+          <DoiMatKhau
+            ten={nhanvienSelected.account}
+            iv={nhanvienSelected.iv}
+            mk={nhanvienSelected.mk}
+          />
+        </div>
+      </div>
     </div>
   );
 };
